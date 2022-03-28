fb30b95bfd89c8cb73b59107f46f4d92af086431
skipp <chanchanakoedtho@gmail.com>
2022-03-29 02:44:13 +0700

    Fix swift package

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
     
          .target(
             name: "SWRevealViewController",
+            dependencies: [],
             path: "Sources",
             sources: ["SWRevealViewController/SWRevealViewController.m"],
             publicHeadersPath: "SWRevealViewController/include",
